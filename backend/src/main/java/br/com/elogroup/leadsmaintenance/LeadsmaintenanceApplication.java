eb0ebab3a37b1f5fd07dfe49157fb8165848aadb
RTAcps <rodrigoandrade20@yahoo.com.br>
2021-11-02 11:03:31 -0300

    Project created

---
+++ backend/src/main/java/br/com/elogroup/leadsmaintenance/LeadsmaintenanceApplication.java
@@ -0,0 +1,13 @@
+package br.com.elogroup.leadsmaintenance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeadsmaintenanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeadsmaintenanceApplication.class, args);
+	}
+
+}
